e0a1273472551c7e474a0aa2f3be23779b9b69dc
GBData94 <gbsourelos94@gmail.com>
2024-08-07 13:57:18 -1000

    app.py file

---
+++ app.py
@@ -0,0 +1,20 @@
+
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('/Users/ginapeccheneda/Desktop/Tripleten/FirstProject/vehicles_us.csv')
+
+st.header('Vehicle Data Analysis')
+
+histogram = px.histogram(df, x='model_year', y='odometer')
+st.plotly_chart(histogram)
+
+histogram2 = px.histogram(df, x='type', y='price')
+st.plotly_chart(histogram2)
+
+scatter_plot = px.scatter(df, x='model_year', y='price')
+st.plotly_chart(scatter_plot)
+
+if st.checkbox('Show scatter plot'):
+    st.plotly_chart(scatter_plot)
