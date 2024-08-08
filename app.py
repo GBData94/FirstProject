4327da57e1acf8d56cdfb3d26cc1361d26d2ea18
GBData94 <gbsourelos94@gmail.com>
2024-08-08 13:24:56 -1000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv('/Users/ginapeccheneda/Desktop/Tripleten/FirstProject/vehicles_us.csv')
+df = pd.read_csv('vehicles_us.csv')
 
 st.header('Vehicle Data Analysis')
 
